8da2490a4fa8534e209c9851d22042b23da94241
Louie-pixel <130101289+Louie-pixel@users.noreply.github.com>
2024-04-17 10:26:27 +0300

    Add files via upload

--- models/amenity.py
+++ models/amenity.py
@@ -1,15 +1,28 @@
 #!/usr/bin/python3
-""" Amenity Module for HBNB project """
-import os
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-
+"""
+    module containing Amenity class
+"""
 from models.base_model import BaseModel, Base
+from models.city import City
+from models.user import User
+from models.place import place_amenity
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from os import environ
+
+storage_engine = environ.get("HBNB_TYPE_STORAGE")
 
 
 class Amenity(BaseModel, Base):
-    """Represents an amenity data set."""
-    __tablename__ = 'amenities'
-    name = Column(
-        String(128), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    """
+        Amenity class
+    """
+
+    if (storage_engine == "db"):
+        __tablename__ = "amenities"
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship(
+            "Place",
+            secondary=place_amenity, back_populates="amenities")
+    else:
+        name = ""
